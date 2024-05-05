a31593861a3df69edc21667455fd94511aced356
Sidhartha Reddy <kattasidharthareddy22it@student.vardhaman.org>
2024-05-05 13:55:39 +0530

    Created the flask app

--- app.py
+++ app.py
@@ -5,12 +5,26 @@ import pandas as pd
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl','rb'))
+scaler = pickle.load(open('Scaler.pkl','rb'))
 
-@app.rout('/')
+@app.route('/')
 def home():
     return render_template('home.html')
 
 @app.route('/predict_api',methods = ['POST'])
+
+def predict_api():
+    data = request.json['data']
+    new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    output = model.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
 
 
 
